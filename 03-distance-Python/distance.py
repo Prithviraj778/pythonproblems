4eff6854504f7f49dfa4f060fffc53b53ef33b34
G.Prithvi raj <71496259+Prithviraj778@users.noreply.github.com>
2021-07-13 17:24:41 +0530

    Update distance.py

--- 03-distance-Python/distance.py
+++ 03-distance-Python/distance.py
@@ -4,6 +4,7 @@
 # and returns the distance between those points as a int.
 
 
-def fun_distance(x1, y1, x2, y2):
-	# your code goes here
-	return 0
+import math
+def fun_distance(x1, y1, x2, y2):    # your code goes here
+    
+    return int((((x2-x1)**2) + ((y2-y1)**2))**0.5)
