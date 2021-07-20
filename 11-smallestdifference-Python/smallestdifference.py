c1b636133260aca9e32ff1c9e4e64d4d3dede194
G.Prithvi raj <71496259+Prithviraj778@users.noreply.github.com>
2021-07-20 16:28:51 +0530

    m6p3

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -4,6 +4,13 @@
 #       assert(smallestDifference([19,2,83,6,27]) == 4)
 # The two closest numbers in that list are 2 and 6, and their difference is 4.
 
-def smallestdifference(a):
-	# Your code goes here
-	pass
+def smallestDifference(a):
+    if(a==[]):
+        return(-1)
+    d=10**20
+    for i in range(len(a)-1):
+        for j in range(i+1,len(a)):
+            if (abs(a[i]-a[j])<d):
+                d=abs(a[i]-a[j])
+                
+    return (d)
