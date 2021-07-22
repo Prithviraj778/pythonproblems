02b20835346735219a04134e6dc5b42d7e72de97
G.Prithvi raj <71496259+Prithviraj778@users.noreply.github.com>
2021-07-22 15:14:04 +0530

    m8P65

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -17,22 +17,36 @@
 # assert(ishappynumber(404) == True)
 # assert(ishappynumber(405) == False)
 
-def Happynum(n):
-    sum = 0
-    while(n!=0):
-        sum += (n%10)**2
-        n//=10
-    if sum == 1:
+def numSquareSum(n):
+    squareSum = 0;
+    while(n>0):
+        squareSum += (n % 10) * (n % 10);
+        n = int(n / 10);
+    return squareSum
+
+# method return true if
+# n is Happy number
+def ishappynumber(n):
+    # initialize slow
+    # and fast by n
+    slow = n;
+    fast = n;
+    while(True):
+
+        # move slow number
+        # by one iteration
+        slow = numSquareSum(slow);
+        # move fast number
+        # by two iteration
+        fast = numSquareSum(numSquareSum(fast));
+        if(slow != fast):
+            continue;
+        else:
+            break;
+
+    # if both number meet at 1,
+    # then return true
+    if (slow == 1):
         return True
-    elif sum<10:
-        return False
     else:
-        return Happynum(sum)
-def nth_happy_number(n):
-    f = 1
-    g = 0
-    while(f<=abs(n)):
-        g+=1
-        if(Happynum(g)):
-            f+=1
-    return g
+        return False
