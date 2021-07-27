bf3eeee207a5dc44564ec1b60d1257d881c20ea9
G.Prithvi raj <71496259+Prithviraj778@users.noreply.github.com>
2021-07-27 15:55:16 +0530

    M11P1

--- 09-median-Python/median.py
+++ 09-median-Python/median.py
@@ -3,6 +3,17 @@
 # which is the value of the middle element, or the average of the two middle elements if there is no single middle 
 # element. If the list is empty, return None.
 
+
+
 def median(a):
-	# your code goes here
-	pass
+    if a==[]:
+        return
+    n=len(a)
+    a.sort()
+    if n%2==0:
+        m1=a[n//2]
+        m2=a[n//2 - 1]
+        M=(m1+m2)/2
+    else:
+        M=a[n//2]
+    return M
